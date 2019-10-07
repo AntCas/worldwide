cfecdcb939695a33fe19ba813b4979e7a1cc0564
Anthony Castrio <anthonycastrio@gmail.com>
2019-10-07 19:30:44 -0400

    hero and hover styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,16 @@ import React from "react"
 import './Hero.scss';
 
 const nextEvent = {
-  date: "Oct 10th",
+  date: "October 10th",
   url: "https://www.mixily.com/event/5853708572761154126"
 }
 
 const Hero = ({ siteTitle }) => (
   <section className="Hero">
     <h2 className="HeroHeader">Worldwide</h2>
-    <a className="CTA" href={nextEvent.url}>Next Meetup: { nextEvent.date }</a>
+    <a className="CTA" href={nextEvent.url}>
+      {`Next Meetup: ${nextEvent.date}`}
+    </a>
   </section>
 )
 
