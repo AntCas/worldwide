2f96b348c440e2ddaa24d95a07a8d072da788be4
Anthony Castrio <anthonycastrio@gmail.com>
2019-10-07 17:05:44 -0400

    add google fonts and leckerli one

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,12 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import './header.scss';
+
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
+  <header className="SiteHeader">
+    <div className="HeaderContent">
+      <h1 className="HeaderLogo">
         <Link to="/" >
           {siteTitle}
         </Link>
