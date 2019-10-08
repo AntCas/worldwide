4a78e3d782fe694442748836422d3e826d26932c
Anthony Castrio <anthonycastrio@gmail.com>
2019-10-08 17:43:36 -0400

    Updates

---
+++ component---src-pages-page-2-js-2cebd664a359bcb6e2a3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{261:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(58),c=a(259),r=a(258);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-2cebd664a359bcb6e2a3.js.map
