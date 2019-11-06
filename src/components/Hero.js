4ad1a4bd802ba8246c6ac50f053d5b880c169da4
Anthony Castrio <anthonycastrio@gmail.com>
2019-11-06 14:25:40 -0500

    add new color to background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,12 +11,14 @@ const nextEvent = {
 
 const Hero = ({ siteTitle }) => (
   <section className="Hero">
-    <h2 className="HeroHeader">Worldwide</h2>
-    <h3 className="Subheader">where indie hackers meet</h3>
-    <Button
-      className="CTA"
-      text={`Next Meetup: ${nextEvent.date}`}
-      link={nextEvent.url} />
+    <div className="HeroInner">
+      <h2 className="HeroHeader">Worldwide</h2>
+      <h3 className="Subheader">where indie hackers meet</h3>
+      <Button
+        className="CTA"
+        text={`Next Meetup: ${nextEvent.date}`}
+        link={nextEvent.url} />
+    </div>
   </section>
 )
 
