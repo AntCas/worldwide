61a5c54f2693fe56017cc333fd16cb545bb66880
Anthony Castrio <anthonycastrio@gmail.com>
2020-03-01 17:07:23 +0100

    implement next event cta

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,14 @@
 import React from "react"
 
-import Button from "./Button";
+import NextEventCTA from "./NextEventCTA";
 
 import './Hero.scss';
 
 const nextEvent = {
-  date: "March 12th",
+  text: "Indie Worldwide - March 2020 Meetup",
+  exactDate: "Thursday, 12 March 2020 - 8:00PM CEST",
+  month: "March",
+  date: "12",
   url: "https://hopin.to/events/indie-worldwide-march-2020-meetup"
 }
 
@@ -15,10 +18,13 @@ const Hero = ({ siteTitle }) => {
       <div className="HeroInner">
         <h2 className="HeroHeader">Worldwide</h2>
         <h3 className="Subheader">where indie hackers meet</h3>
-        <Button
-          className="CTA"
-          text={`Next Meetup: ${nextEvent.date}`}
-          link={nextEvent.url} />
+        <NextEventCTA
+          text={nextEvent.text}
+          exactDate={nextEvent.exactDate}
+          month={nextEvent.month} 
+          date={nextEvent.date}
+          link={nextEvent.url}
+        />
       </div>
     </section>
   )
---
+++ src/components/NextEventCTA.js
@@ -0,0 +1,29 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import './NextEventCTA.scss';
+import ArrowRight from "../assets/arrow-right.svg";
+
+const Button = ({text, exactDate, month, date, link, className}) => (
+  <a className={`NextEventCTA ${className}`} href={link}>
+    <div className="NextEvent">NEXT EVENT</div>
+    <div className="EventButton">
+      <div className="DateIcon">
+        <div className="Month">{month}</div>
+        <div className="Date">{date}</div>
+      </div>
+      <div className="Info">
+        <div className="EventTitle">{text}</div>
+        <div className="ExactDate">{exactDate}</div>
+      </div>
+      <ArrowRight className="Arrow" />
+    </div>
+  </a>
+)
+
+Button.propTypes = {
+  text: PropTypes.node.isRequired,
+  link: PropTypes.node.isRequired,
+}
+
+export default Button;
