a7bda5135b4a54eb5a1f8514d7de85d536b9795b
Anthony Castrio <anthonycastrio@gmail.com>
2020-03-02 16:53:07 +0100

    add real testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,33 +1,45 @@
 import React, { useState } from "react"
 
+import Dan from "../images/testimonials/Dan.png";
+import Dru from "../images/testimonials/Dru.jpg";
+import Marc from "../images/testimonials/Marc.jpg";
+import Volkan from "../images/testimonials/Volkan.jpg";
+
 import './Testimonials.scss';
 
 const testimonials = [
   {
-    testimonial: "Probably the best meet-up I’ve ever been a part of. The community just rocks. I’ll definitely be attending the next event.",
-    name: "Bob Job",
-    bio: "Founder of CoolCo",
-    image: "",
-    backlink: "https://google.com"
+    testimonial: "Indie Worldwide has been a fantastic experience for me as a community of entrepreneurs and self starters. Awesome to get feedback, get asked hard questions, and get that extra push to keep going.",
+    name: "Dan Bogachek",
+    bio: "Founder, Handwrite",
+    image: Dan,
+    backlink: "https://handwrite.io/"
   },
   {
-    testimonial: "Probably the best meet-up I’ve ever been a part of. The community just rocks. I’ll definitely be attending the next event.",
-    name: "Bob Job",
-    bio: "Founder of CoolCo",
-    image: "",
-    backlink: "https://google.com"
+    testimonial: "...Indie Hackers from around the world bring unique perspectives while showing that we go through common struggles as makers. I was ready for the next event before the first one was over.",
+    name: "Dru Riley",
+    bio: "Founder, StrongStack",
+    image: Dru,
+    backlink: "https://strongstack.io/"
   },
   {
-    testimonial: "Probably the best meet-up I’ve ever been a part of. The community just rocks. I’ll definitely be attending the next event.",
-    name: "Bob Job",
-    bio: "Founder of CoolCo",
-    image: "",
-    backlink: "https://google.com"
+    testimonial: "Coffee, exercise, and IH Worldwide are my tools for being a more productive indie hacker. Whenever I get stuck on something, I find helpful feedback and tips. It's a great community!",
+    name: "Marc Lavigne-Gagnon",
+    bio: "Founder, PaleoTrack",
+    image: Marc,
+    backlink: "https://marc.lavigne-gagnon.com/"
+  },
+  {
+    testimonial: "As a founder of an early stage remote company Indie Worldwide gives me the opportunity to talk to peers that I normally wouldn't get. The calls are great...",
+    name: "Volkan Kaya",
+    bio: "Founder, Versoly",
+    image: Volkan,
+    backlink: "https://versoly.com/"
   },
 ]
 
 const Testimonials = () => {
-  const [selected, setSelected] = useState(1);
+  const [selected, setSelected] = useState(Math.floor(Math.random() * Math.floor(testimonials.length)));
 
   const Items = testimonials.map((data, i) => {
     return (
@@ -38,7 +50,7 @@ const Testimonials = () => {
     >
       <p className="Text">{`"${data.testimonial}"`}</p>
       <a className="Attribution" href={data.backlink}>
-        <div className="Image">image</div>
+        <img alt={data.name} src={data.image} className="Image" />
         <div className="Info">
           <div className="Name">{data.name}</div>
           <div className="Bio">{data.bio}</div>
