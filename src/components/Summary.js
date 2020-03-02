e52a2ecdfa5cea36e82fa4bad527137165bdd4e1
Anthony Castrio <anthonycastrio@gmail.com>
2020-03-02 13:49:45 +0100

    update Summary copy

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,7 +4,7 @@ import './Summary.scss';
 const Summary = () => (
   <section className="Summary">
     <h2 className="Title">What is Worldwide?</h2>
-    <p className="Description">{`A community for makers who can't get to an in-person meetup group or want to expand their circle beyond their local area. We host online video chats where you can make friends, meet potential co-founders or clients, and get advice. 100% online.`}</p>
+    <p className="Description">{`A community for makers who can't get to an in-person meetup group or want to expand their circle beyond their local area. We host online video chats where you can make friends, meet co-founders, find new clients, and get advice. 100% online.`}</p>
   </section>
 )
 
