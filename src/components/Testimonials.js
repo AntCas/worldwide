70496118112fbd2225fccc00294a2bdf2f13d74f
Anthony Castrio <anthonycastrio@gmail.com>
2020-06-15 14:43:36 -0400

    make testimonials shorter

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -15,35 +15,35 @@ import './Testimonials.scss';
 
 const testimonials = [
   {
-    testimonial: "Indie Worldwide has been a fantastic experience for me as a community of entrepreneurs and self starters. Awesome to get feedback, get asked hard questions, and get that extra push to keep going.",
+    testimonial: "Get feedback, get asked hard questions, and get that extra push to keep going.",
     name: "Dan Bogachek",
     bio: "Founder, Handwrite",
     image: Dan,
     backlink: "https://handwrite.io/"
   },
   {
-    testimonial: "...Indie Hackers from around the world bring unique perspectives while showing that we go through common struggles as makers. I was ready for the next event before the first one was over.",
+    testimonial: "I was ready for the next event before the first one was over.",
     name: "Dru Riley",
     bio: "Founder, StrongStack",
     image: Dru,
     backlink: "https://strongstack.io/"
   },
   {
-    testimonial: "Coffee, exercise, and IH Worldwide are my tools for being a more productive indie hacker. Whenever I get stuck on something, I find helpful feedback and tips. It's a great community!",
+    testimonial: "Coffee, exercise, and IH Worldwide are my tools for being a more productive indie hacker.",
     name: "Marc Lavigne-Gagnon",
     bio: "Founder, PaleoTrack",
     image: Marc,
     backlink: "https://marc.lavigne-gagnon.com/"
   },
   {
-    testimonial: "As a founder of an early stage remote company Indie Worldwide gives me the opportunity to talk to peers that I normally wouldn't get. The calls are great and I love how passionate Anthony is for the Indie Hackers space.",
+    testimonial: "Indie Worldwide gives me the opportunity to talk to peers that I normally wouldn't get.",
     name: "Volkan Kaya",
     bio: "Founder, Versoly",
     image: Volkan,
     backlink: "https://versoly.com/"
   },
   {
-    testimonial: "Worldwide is an excellent resource for bootstrapping your company! It's an amazing group of talented and experienced people helping each other move their businesses forward. I finish every session feeling energized with insightful and actionable next steps.",
+    testimonial: "I finish every session feeling energized with insightful and actionable next steps.",
     name: "Heliodor Jalba",
     bio: "Founder, HostedMetrics.com",
     image: Heliodor,
@@ -91,7 +91,7 @@ const Testimonials = () => {
     afterChange: current => setSelected(current),
     responsive: [
       {
-        breakpoint: 992,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 2,
         }
