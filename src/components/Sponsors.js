64e471724610cf77a49f66721f26afa56279347c
Anthony Castrio <anthonycastrio@gmail.com>
2021-04-24 12:27:46 -0500

    add sponsorship cta

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -5,13 +5,17 @@ import './Sponsors.scss';
 const Sponsors = ({sponsors}) => {
   return (
     <section className="Sponsors">
-      <h2 className="Title">Sponsored By</h2>
+      <h3 className="Title">Sponsored By</h3>
       <div className="SponsorLogos"> 
         {sponsors && sponsors.map((sponsor, idx) => 
           <a key={idx} className="Sponsor" href={sponsor.link}>
             <img alt={sponsor.name} src={sponsor.image} className="Image" />
           </a>
         )}
+      </div>
+      <h4 className="Subtitle">Interested in Sponsoring?</h4>
+      <div className="SponsorshipCTA">
+        <a className="Button" href="mailto:sponsorships@indieworldwide.co">{"Contact Us"}</a>
       </div>
     </section>
   );
