6fbae3a7ad0b0e3902bf96f067bd5acf67d9d7a5
Anthony Castrio <anthonycastrio@gmail.com>
2021-12-28 18:21:13 -0500

    add better error handling to email signup and improve UX

--- src/components/EmailListSignup.js
+++ src/components/EmailListSignup.js
@@ -9,6 +9,7 @@ const EmailListSignup = ({headline}) => {
   const [email, setEmail] = useState();
   const [result, setResult] = useState();
   const [message, setMessage] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   const resetState = () => {
     setResult('');
@@ -17,15 +18,24 @@ const EmailListSignup = ({headline}) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { result, msg } = await addToMailchimp(email)
-    setResult(result);
-    if (msg.includes('subscribed')) setMessage('This email is already subscribed.');
-    else setMessage(msg.replace(/\d|( - )/g, ''));
+    setIsLoading(true);
+    setResult('');
+    try {
+      const { result, msg } = await addToMailchimp(email)
+      setResult(result);
+      if (msg.includes('subscribed')) setMessage('This email is already subscribed.');
+      else setMessage(msg.replace(/\d|( - )/g, ''));
 
-    if (result === 'success') {
-      setEmail('');
-      window.history.pushState({}, '', '/');
-      window.location.replace('/post-signup');
+      if (result === 'success') {
+        setEmail('');
+        window.history.pushState({}, '', '/');
+        window.location.replace('/post-signup');
+      }
+    } catch (e) {
+      setResult('error');
+      setMessage("Ouch, hit an error. This form breaks on browsers with strict privacy settings.");
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -39,18 +49,23 @@ const EmailListSignup = ({headline}) => {
   return (
     <section className="EmailListSignup" id="Subscribe">
       <h2 className="EmailSignupHeader">{headline}</h2>
-      <form onSubmit={handleSubmit} className={`${result === 'error' ? 'Error' : ''}`}>
-        <input
-          className="EmailInput"
-          type="text"
-          name="email"
-          value={email}
-          onChange={handleInputChange}
-          placeholder="Enter your email"
-        />
-        <button className="Submit" type="submit">
-          <ArrowRight className="Arrow" />
-        </button>
+      <form
+        onSubmit={handleSubmit}
+        className={`${result === 'error' ? 'Error' : ''} ${isLoading && 'Disabled'}`}
+      >
+        <fieldset disabled={isLoading}>
+          <input
+            className="EmailInput"
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+            placeholder="Enter your email"
+          />
+          <button className="Submit" type="submit">
+            <ArrowRight className="Arrow" />
+          </button>
+        </fieldset>
       </form>
       <p className="Message">{message}</p>
     </section>
