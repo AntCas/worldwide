4838f92119c55c74c1d2e5878131b6ee59a0d708
Anthony Castrio <anthonycastrio@gmail.com>
2022-01-01 16:13:22 -0500

    update twitter preview format

--- src/components/seo.js
+++ src/components/seo.js
@@ -61,7 +61,7 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
