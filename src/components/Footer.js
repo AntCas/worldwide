1a43f186cee268d9d564d4b454712eac10ca1e78
Anthony Castrio <anthonycastrio@gmail.com>
2022-02-06 17:13:08 -0500

    add link to calendar hosted on Notion to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,6 +18,7 @@ const Footer = ({ siteTitle }) => (
         <Link className="SiteLink" to="/podcast">Podcast</Link>
         <Link className="SiteLink" to="/post-signup">Getting Started</Link>
         <Link className="SiteLink" to="/wall-of-love">Wall of Love</Link>
+        <a className="SiteLink" href="https://indieworldwide.notion.site/16df0d3322e6496ba524737a647f3639?v=f18f5cccb2fd428580cace4d7d292be2">Events Calendar</a>
       </div>
       <a className="Badge" href="https://www.unita.co/communities/indie-worldwide/">
         <UnitaBadge alt="Unita Top 10 Community Badge" />
