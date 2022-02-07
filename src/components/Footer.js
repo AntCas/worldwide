f57fe2508a9fcb9511beb333a3b978e0d01b4f22
Anthony Castrio <anthonycastrio@gmail.com>
2022-02-07 18:51:21 -0500

    add slack community page to footer and sort alphabetically

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,12 +14,13 @@ const Footer = ({ siteTitle }) => (
     <div className="FooterContent">
       <div className="FooterLeft">
         <Link className="SiteLink" to="/community-guidelines">Community Guidelines</Link>
+        <a className="SiteLink" href="https://indieworldwide.notion.site/16df0d3322e6496ba524737a647f3639?v=f18f5cccb2fd428580cace4d7d292be2">Events Calendar</a>
         <Link className="SiteLink" to="/founders-club">Founders Club</Link>
+        <Link className="SiteLink" to="/post-signup">Getting Started</Link>
         <Link className="SiteLink" to="/free-resources-for-startup-founders">Partner Deals - Free Resources</Link>
         <Link className="SiteLink" to="/podcast">Podcast</Link>
-        <Link className="SiteLink" to="/post-signup">Getting Started</Link>
+        <Link className="SiteLink" to="/community-for-bootstrapped-startup-founders">Slack Community</Link>
         <Link className="SiteLink" to="/wall-of-love">Wall of Love</Link>
-        <a className="SiteLink" href="https://indieworldwide.notion.site/16df0d3322e6496ba524737a647f3639?v=f18f5cccb2fd428580cace4d7d292be2">Events Calendar</a>
       </div>
       <a className="Badge" href="https://www.unita.co/communities/indie-worldwide/">
         <UnitaBadge alt="Unita Top 10 Community Badge" />
