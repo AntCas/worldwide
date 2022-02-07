7876d743cab2a44980ba6fa2fae7fe3a203d75d8
Anthony Castrio <anthonycastrio@gmail.com>
2022-02-07 17:27:16 -0500

    ass sitemap w default config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     ogImage: `/images/social.png`
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
